8ec7c8046171b478e7c0ffabfa43ded776bef2bd
frenisko <155766172+frenisko@users.noreply.github.com>
2024-01-05 21:14:37 +0300

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,9 @@
+let con = confirm("Вам есть 18?")
+if(con === true){
+    alert("Добро пожаловать")
+} else if(con === false){
+    // alert("Вход запрещен")
+    for(i = 0; i < 9999999999;i++){
+        alert("Вход запрещен")
+    }
+}
